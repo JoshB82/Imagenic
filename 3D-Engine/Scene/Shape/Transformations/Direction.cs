e1ef87cc8a1ace6c7cdb5514e7b1ee4c852e9d52
JoshB82 <jbryantjersey@hotmail.co.uk>
2020-07-23 18:10:29 +0100

    Small fix

--- 3D-Engine/Scene/Shape/Transformations/Direction.cs
+++ 3D-Engine/Scene/Shape/Transformations/Direction.cs
@@ -8,20 +8,20 @@
         /// <param name="direction_up">The up direction.</param>
         /// <param name="direction_right">The right direction.</param>
         /// <returns>The forward direction.</returns>
-        public static Vector3D Calculate_Direction_Forward(Vector3D direction_up, Vector3D direction_right) => direction_up.Cross_Product(direction_right);
+        public static Vector3D Calculate_Direction_Forward(Vector3D direction_up, Vector3D direction_right) => direction_right.Cross_Product(direction_up);
         /// <summary>
         /// Calculates the up direction given the right direction and the forward direction.
         /// </summary>
         /// <param name="direction_right">The right direction.</param>
         /// <param name="direction_forward">The forward direction.</param>
         /// <returns>The up direction.</returns>
-        public static Vector3D Calculate_Direction_Up(Vector3D direction_right, Vector3D direction_forward) => direction_right.Cross_Product(direction_forward);
+        public static Vector3D Calculate_Direction_Up(Vector3D direction_right, Vector3D direction_forward) => direction_forward.Cross_Product(direction_right);
         /// <summary>
         /// Calculates the right direction given the forward direction and the up direction.
         /// </summary>
         /// <param name="direction_forward">The forward direction.</param>
         /// <param name="direction_up">The up direction.</param>
         /// <returns>The right direction.</returns>
-        public static Vector3D Calculate_Direction_Right(Vector3D direction_forward, Vector3D direction_up) => direction_forward.Cross_Product(direction_up);
+        public static Vector3D Calculate_Direction_Right(Vector3D direction_forward, Vector3D direction_up) => direction_up.Cross_Product(direction_forward);
     }
 }
