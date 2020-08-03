4445f4c1fd2993bcea1d4e706a9391826b10bfb9
JoshB82 <jbryantjersey@hotmail.co.uk>
2020-08-03 15:59:56 +0100

    Restructuring

--- 3D-Engine/Scene/Common.cs
+++ 3D-Engine/Scene/Common.cs
@@ -14,14 +14,14 @@ namespace _3D_Engine
         }
 
         /// <summary>
-        /// Creates axes (arrows) starting from (0, 0, 0) and adds them to the <see cref="Scene"/>.
+        /// Creates axes (<see cref="Arrow"/>) starting from (0, 0, 0) and adds them to the <see cref="Scene"/>.
         /// </summary>
         public void Create_Axes()
         {
             int resolution = 30, body_radius = 50, tip_radius = 100, tip_length = 50;
-            Arrow x_axis = new Arrow(new Vector3D(0, 0, 0), new Vector3D(250, 0, 0), body_radius, tip_length, tip_radius, resolution) { Edge_Colour = Color.Red };
-            Arrow y_axis = new Arrow(new Vector3D(0, 0, 0), new Vector3D(0, 250, 0), body_radius, tip_length, tip_radius, resolution) { Edge_Colour = Color.Green };
-            Arrow z_axis = new Arrow(new Vector3D(0, 0, 0), new Vector3D(0, 0, 250), body_radius, tip_length, tip_radius, resolution) { Edge_Colour = Color.Blue };
+            Arrow x_axis = new Arrow(Vector3D.Zero, new Vector3D(250, 0, 0), body_radius, tip_length, tip_radius, resolution) { Edge_Colour = Color.Red };
+            Arrow y_axis = new Arrow(Vector3D.Zero, new Vector3D(0, 250, 0), body_radius, tip_length, tip_radius, resolution) { Edge_Colour = Color.Green };
+            Arrow z_axis = new Arrow(Vector3D.Zero, new Vector3D(0, 0, 250), body_radius, tip_length, tip_radius, resolution) { Edge_Colour = Color.Blue };
 
             Add(x_axis); Add(y_axis); Add(z_axis);
         }
