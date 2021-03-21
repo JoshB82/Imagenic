b00fb92fcede537e0438cc3b29fb8898b65e589b
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-03-21 20:22:13 +0000

    Updated verbosity enum

--- 3D-Engine/Enums/Verbosity.cs
+++ 3D-Engine/Enums/Verbosity.cs
@@ -1,13 +1,37 @@
-﻿namespace _3D_Engine.Enums
+﻿/*
+ *       -3D-Engine-
+ *     (c) Josh Bryant
+ * https://joshdbryant.com
+ *
+ * Full license is available in the GitHub repository:
+ * https://github.com/JoshB82/3D-Engine/blob/master/LICENSE
+ *
+ * Code description for this file:
+ * Defines an enum for options describing the verbosity of output.
+ */
+
+namespace _3D_Engine.Enums
 {
     /// <summary>
-    /// Options for how verbose output should be.??????????????
+    /// Options describing the verbosity of output.
     /// </summary>
     public enum Verbosity
     {
+        /// <summary>
+        /// Indicates that output should be empty.
+        /// </summary>
         None,
+        /// <summary>
+        /// Indicates that output should be short and concise.
+        /// </summary>
         Brief,
+        /// <summary>
+        /// Indicates that output should be comprehensive and thorough.
+        /// </summary>
         Detailed,
+        /// <summary>
+        /// Indicates that output should contain all necessary information.
+        /// </summary>
         All
     }
 }
