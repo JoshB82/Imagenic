d67fc9493d773aaae7d43d07afe0bd34a1f87c5d
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-03-27 16:05:40 +0000

    Updates

--- Statistics Demo/MainForm.Designer.cs
+++ Statistics Demo/MainForm.Designer.cs
@@ -29,20 +29,87 @@ namespace Statistics_Demo
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.viewAllButton = new System.Windows.Forms.Button();
+            this.noSceneObjectsValueLabel = new System.Windows.Forms.Label();
+            this.noSceneObjectsTextLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 70);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(788, 258);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.viewAllButton);
+            this.panel1.Controls.Add(this.noSceneObjectsValueLabel);
+            this.panel1.Controls.Add(this.noSceneObjectsTextLabel);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 52);
+            this.panel1.TabIndex = 1;
+            // 
+            // viewAllButton
+            // 
+            this.viewAllButton.Location = new System.Drawing.Point(670, 13);
+            this.viewAllButton.Name = "viewAllButton";
+            this.viewAllButton.Size = new System.Drawing.Size(94, 29);
+            this.viewAllButton.TabIndex = 2;
+            this.viewAllButton.Text = "View All";
+            this.viewAllButton.UseVisualStyleBackColor = true;
+            this.viewAllButton.Click += new System.EventHandler(this.viewAllButton_Click);
+            // 
+            // noSceneObjectsValueLabel
+            // 
+            this.noSceneObjectsValueLabel.AutoSize = true;
+            this.noSceneObjectsValueLabel.Location = new System.Drawing.Point(164, 17);
+            this.noSceneObjectsValueLabel.Name = "noSceneObjectsValueLabel";
+            this.noSceneObjectsValueLabel.Size = new System.Drawing.Size(17, 20);
+            this.noSceneObjectsValueLabel.TabIndex = 1;
+            this.noSceneObjectsValueLabel.Text = "0";
+            // 
+            // noSceneObjectsTextLabel
+            // 
+            this.noSceneObjectsTextLabel.AutoSize = true;
+            this.noSceneObjectsTextLabel.Location = new System.Drawing.Point(12, 17);
+            this.noSceneObjectsTextLabel.Name = "noSceneObjectsTextLabel";
+            this.noSceneObjectsTextLabel.Size = new System.Drawing.Size(146, 20);
+            this.noSceneObjectsTextLabel.TabIndex = 0;
+            this.noSceneObjectsTextLabel.Text = "No. of SceneObjects:";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox);
             this.Name = "MainForm";
             this.Text = "Statistics Demo";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label noSceneObjectsTextLabel;
+        private System.Windows.Forms.Label noSceneObjectsValueLabel;
+        private System.Windows.Forms.Button viewAllButton;
     }
 }
 
--- Statistics Demo/MainForm.cs
+++ Statistics Demo/MainForm.cs
@@ -1,21 +1,169 @@
-﻿using System;
+﻿using _3D_Engine.Maths.Vectors;
+using _3D_Engine.SceneObjects.Groups;
+using _3D_Engine.SceneObjects.Meshes.OneDimension;
+using _3D_Engine.SceneObjects.Meshes.ThreeDimensions;
+using _3D_Engine.SceneObjects.Meshes.TwoDimensions;
+using _3D_Engine.SceneObjects.RenderingObjects.Cameras;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Statistics_Demo
 {
     public partial class MainForm : Form
     {
+        private readonly OrthogonalCamera camera;
+        private Group sceneToRender;
+        private List<Keys> keysPressed = new();
+        private int noSceneObjects = 0;
+
         public MainForm()
         {
             InitializeComponent();
+
+            Group scene = new();
+
+            scene.Add(WorldPoint.ZeroOrigin);
+            scene.Add(Arrow.Axes);
+
+            Circle circle = new(new Vector3D(50, 50, 50), Vector3D.UnitX, Vector3D.UnitY, 25, 50);
+            scene.Add(circle);
+
+            camera = new OrthogonalCamera(new Vector3D(500, 50, 200), scene.SceneObjects[0], Vector3D.UnitY, pictureBox.Width / 10f, pictureBox.Height / 10f, 50, 750, pictureBox.Width, pictureBox.Height);
+            scene.Add(camera);
+
+            noSceneObjects = 4;
+            noSceneObjectsValueLabel.Text = noSceneObjects.ToString();
+
+            //pictureBox.Image = camera.Render(scene);
+            sceneToRender = scene;
+
+            Thread thread = new(Loop) { IsBackground = true };
+            thread.Start();
         }
 
+        private void Loop()
+        {
+            bool running = true;
+
+            const int maxFramePerSecond = 60, maxUpdatesPerSecond = 60;
+            const long frameMinimumTime = 1000 / maxFramePerSecond; //(?)
+            const long updateMinimumTime = 1000 / maxUpdatesPerSecond; // ?
+
+            int noFrames = 0, noUpdates = 0, timer = 1;
+
+            long nowTime, deltaTime, frameTime = 0, updateTime = 0;
+
+            Stopwatch sw = Stopwatch.StartNew();
+            long startTime = sw.ElapsedMilliseconds;
+
+            while (running)
+            {
+                nowTime = sw.ElapsedMilliseconds;
+                deltaTime = nowTime - startTime;
+                startTime = nowTime;
+
+                frameTime += deltaTime;
+                updateTime += deltaTime;
+
+                if (frameTime >= frameMinimumTime)
+                {
+                    this.Invoke((MethodInvoker)(() => pictureBox.Image = camera.Render(sceneToRender)));
+                    noFrames++; //?
+                    frameTime -= frameMinimumTime;
+                }
+
+                if (updateTime >= updateMinimumTime)
+                {
+                    //Update_Position(); uncomment
+                    noUpdates++; //?
+                    updateTime -= updateMinimumTime;
+                }
+
+                if (nowTime >= 1000 * timer)
+                {
+                    this.Invoke((MethodInvoker)(() => this.Text = $"Interactive Demo - FPS: {noFrames}, UPS: {noUpdates}"));
+                    noFrames = 0; noUpdates = 0;
+                    timer += 1;
+                }
+
+                CheckKeyboard(updateTime);
+            }
+        }
+
+        private void CheckKeyboard(long updateTime)
+        {
+            const float cameraPanDampener = 0.0008f, cameraTiltDampener = 0.000001f;
+
+            for (int i = 0; i < keysPressed.Count; i++)
+            {
+                switch (keysPressed[i])
+                {
+                    case Keys.W:
+                        // Pan forward
+                        camera.PanForward(cameraPanDampener * updateTime);
+                        break;
+                    case Keys.A:
+                        // Pan left
+                        camera.PanLeft(cameraPanDampener * updateTime);
+                        break;
+                    case Keys.D:
+                        // Pan right
+                        camera.PanRight(cameraPanDampener * updateTime);
+                        break;
+                    case Keys.S:
+                        // Pan backward
+                        camera.PanBackward(cameraPanDampener * updateTime);
+                        break;
+                    case Keys.Q:
+                        // Pan up
+                        camera.PanUp(cameraPanDampener * updateTime);
+                        break;
+                    case Keys.E:
+                        // Pan down
+                        camera.PanDown(cameraPanDampener * updateTime);
+                        break;
+                    case Keys.I:
+                        // Rotate up
+                        camera.RotateUp(cameraTiltDampener * updateTime);
+                        break;
+                    case Keys.J:
+                        // Rotate left
+                        camera.RotateLeft(cameraTiltDampener * updateTime);
+                        break;
+                    case Keys.L:
+                        // Rotate right
+                        camera.RotateRight(cameraTiltDampener * updateTime);
+                        break;
+                    case Keys.K:
+                        // Rotate down
+                        camera.RotateDown(cameraTiltDampener * updateTime);
+                        break;
+                    case Keys.U:
+                        // Roll left
+                        camera.RollLeft(cameraTiltDampener * updateTime);
+                        break;
+                    case Keys.O:
+                        // Roll right
+                        camera.RollRight(cameraTiltDampener * updateTime);
+                        break;
+                }
+            }
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            keysPressed.Add(e.KeyCode);
+            keysPressed = keysPressed.Distinct().ToList();
+        }
+
+        private void MainForm_KeyUp(object sender, KeyEventArgs e) => keysPressed.Remove(e.KeyCode);
+
+        private void viewAllButton_Click(object sender, System.EventArgs e)
+        {
+
+        }
     }
 }
