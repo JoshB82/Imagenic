1e321cac63b7f16e7be860163f9b23da9eb47606
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-08-08 17:12:16 +0100

    Updates

--- 3D-Engine/Maths/Orientation.cs
+++ 3D-Engine/Maths/Orientation.cs
@@ -7,9 +7,9 @@ namespace _3D_Engine.Maths
     {
         #region Fields and Properties
 
-        public Vector3D DirectionForward { get; set; }
-        public Vector3D DirectionUp { get; set; }
-        public Vector3D DirectionRight { get; set; }
+        public Vector3D DirectionForward { get; private set; }
+        public Vector3D DirectionUp { get; private set; }
+        public Vector3D DirectionRight { get; private set; }
 
         #endregion
 
@@ -43,6 +43,31 @@ namespace _3D_Engine.Maths
                 DirectionUp = Transform.CalculateDirectionUp(directionRight, directionForward),
                 DirectionRight = directionRight
             };
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void SetDirectionForwardUp(Vector3D directionForward, Vector3D directionUp)
+        {
+            DirectionForward = directionForward;
+            DirectionUp = directionUp;
+            DirectionRight = Transform.CalculateDirectionRight(directionForward, directionUp);
+        }
+
+        public void SetDirectionUpRight(Vector3D directionUp, Vector3D directionRight)
+        {
+            DirectionForward = Transform.CalculateDirectionForward(directionUp, directionRight);
+            DirectionUp = directionUp;
+            DirectionRight = directionRight;
+        }
+
+        public void SetDirectionRightForward(Vector3D directionRight, Vector3D directionForward)
+        {
+            DirectionForward = directionForward;
+            DirectionUp = Transform.CalculateDirectionUp(directionRight, directionForward);
+            DirectionRight = directionRight;
         }
 
         #endregion
