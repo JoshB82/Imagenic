af9ceb78adf01f7372711a87974eef6d7e3643d6
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-08-28 17:01:18 +0100

    Updates

--- 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Cuboids/Cube.cs
+++ 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Cuboids/Cube.cs
@@ -41,29 +41,6 @@ namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions
             }
         }
 
-        // Structure
-        internal static readonly IList<Vertex> ModelVertices = new List<Vertex>
-        {
-            new Vertex(new Vector4D(0, 0, 0, 1), new Vector3D(,,)), // 0
-            new Vertex(new Vector4D(1, 0, 0, 1), new Vector3D(,,)), // 1
-            new Vertex(new Vector4D(1, 1, 0, 1), new Vector3D(,,)), // 2
-            new Vertex(new Vector4D(0, 1, 0, 1), new Vector3D(,,)), // 3
-            new Vertex(new Vector4D(0, 0, 1, 1), new Vector3D(,,)), // 4
-            new Vertex(new Vector4D(1, 0, 1, 1), new Vector3D(,,)), // 5
-            new Vertex(new Vector4D(1, 1, 1, 1), new Vector3D(,,)), // 6
-            new Vertex(new Vector4D(0, 1, 1, 1), new Vector3D(,,)) // 7
-        };
-
-        internal static readonly IList<Edge> MeshEdges = new List<Edge>
-        {
-
-        };
-
-        internal static readonly IList<Face> MeshFaces = new List<Face>
-        {
-
-        };
-
         #endregion
 
         #region Constructors
--- 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Cuboids/ModelCuboid.cs
+++ 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Cuboids/ModelCuboid.cs
@@ -1,10 +1,33 @@
-﻿namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Cuboids
+﻿using System.Collections.Generic;
+
+namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Cuboids
 {
     public sealed partial class ModelCuboid
     {
         #region Fields and Properties
 
+        // Structure
+        internal static readonly IList<Vertex> ModelVertices = new List<Vertex>
+        {
+            new Vertex(new Vector4D(0, 0, 0, 1), new Vector3D(,,)), // 0
+            new Vertex(new Vector4D(1, 0, 0, 1), new Vector3D(,,)), // 1
+            new Vertex(new Vector4D(1, 1, 0, 1), new Vector3D(,,)), // 2
+            new Vertex(new Vector4D(0, 1, 0, 1), new Vector3D(,,)), // 3
+            new Vertex(new Vector4D(0, 0, 1, 1), new Vector3D(,,)), // 4
+            new Vertex(new Vector4D(1, 0, 1, 1), new Vector3D(,,)), // 5
+            new Vertex(new Vector4D(1, 1, 1, 1), new Vector3D(,,)), // 6
+            new Vertex(new Vector4D(0, 1, 1, 1), new Vector3D(,,)) // 7
+        };
 
+        internal static readonly IList<Edge> MeshEdges = new List<Edge>
+        {
+
+        };
+
+        internal static readonly IList<Face> MeshFaces = new List<Face>
+        {
+
+        };
 
         #endregion
     }
