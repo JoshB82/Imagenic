b70930fdc9335e6acf88b9423006dd28e1e24521
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-08-28 19:35:11 +0100

    Updates

--- 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Cuboids/ModelCuboid.cs
+++ 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Cuboids/ModelCuboid.cs
@@ -1,4 +1,5 @@
 ﻿using _3D_Engine.Entities.SceneObjects.Meshes.Components;
+using _3D_Engine.Maths.Vectors;
 using System;
 using System.Collections.Generic;
 
@@ -9,21 +10,22 @@ namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Cuboids
         #region Fields and Properties
 
         // Structure
-        internal static readonly IList<Vertex> ModelVertices = new List<Vertex>
-        {
-            new Vertex(new Vector4D(0, 0, 0, 1), new Vector3D(,,)), // 0
-            new Vertex(new Vector4D(1, 0, 0, 1), new Vector3D(,,)), // 1
-            new Vertex(new Vector4D(1, 1, 0, 1), new Vector3D(,,)), // 2
-            new Vertex(new Vector4D(0, 1, 0, 1), new Vector3D(,,)), // 3
-            new Vertex(new Vector4D(0, 0, 1, 1), new Vector3D(,,)), // 4
-            new Vertex(new Vector4D(1, 0, 1, 1), new Vector3D(,,)), // 5
-            new Vertex(new Vector4D(1, 1, 1, 1), new Vector3D(,,)), // 6
-            new Vertex(new Vector4D(0, 1, 1, 1), new Vector3D(,,)) // 7
-        };
+        internal static readonly IList<Vertex> ModelVertices = GenerateModelVertices();
 
         internal static List<Vertex> GenerateModelVertices()
         {
             float radical = MathF.Sqrt(3) / 3;
+            return new List<Vertex>
+            {
+                new Vertex(new Vector4D(0, 0, 0, 1), new Vector3D(,,)), // 0
+                new Vertex(new Vector4D(1, 0, 0, 1), new Vector3D(,,)), // 1
+                new Vertex(new Vector4D(1, 1, 0, 1), new Vector3D(,,)), // 2
+                new Vertex(new Vector4D(0, 1, 0, 1), new Vector3D(,,)), // 3
+                new Vertex(new Vector4D(0, 0, 1, 1), new Vector3D(,,)), // 4
+                new Vertex(new Vector4D(1, 0, 1, 1), new Vector3D(,,)), // 5
+                new Vertex(new Vector4D(1, 1, 1, 1), new Vector3D(,,)), // 6
+                new Vertex(new Vector4D(0, 1, 1, 1), new Vector3D(,,)) // 7
+            };
         }
 
         internal static readonly IList<Edge> MeshEdges = new List<Edge>
