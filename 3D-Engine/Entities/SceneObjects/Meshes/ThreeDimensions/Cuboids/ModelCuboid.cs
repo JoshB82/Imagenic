c4f5a57e8a7e205140bf1e73bc9284e61fc657a5
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-08-28 16:54:54 +0100

    Updates

---
+++ 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Cuboids/ModelCuboid.cs
@@ -0,0 +1,11 @@
+﻿namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Cuboids
+{
+    public sealed partial class ModelCuboid
+    {
+        #region Fields and Properties
+
+
+
+        #endregion
+    }
+}
