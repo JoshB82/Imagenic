5dc4dbe547a046c51914f8262bc332d02bb2fd9f
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-10-22 10:51:55 +0100

    Updates

--- 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Cuboids/Cube.cs
+++ 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Cuboids/Cube.cs
@@ -15,7 +15,6 @@ using _3D_Engine.Entities.SceneObjects.Meshes.Components.Edges;
 using _3D_Engine.Entities.SceneObjects.Meshes.Components.Faces;
 using _3D_Engine.Maths;
 using _3D_Engine.Maths.Vectors;
-using System.Collections.Generic;
 
 namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Cuboids
 {
@@ -70,7 +69,7 @@ namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Cuboids
         /// Creates a textured <see cref="Cube"/> mesh, specifying a single <see cref="Texture"/> for all sides.
         /// </summary>
         /// <param name="worldOrigin">The position of the <see cref="Cube"/> in world space.</param>
-        /// <param name="worldOrientation"></param>
+        /// <param name="worldOrientation">The orientation of the <see cref="Cube"/> in world space.</param>
         /// <param name="sideLength">The length of each side.</param>
         /// <param name="texture">The <see cref="Texture"/> that defines what to draw on each surface of the <see cref="Cube"/>.</param>
         public Cube(Vector3D worldOrigin,
@@ -90,7 +89,7 @@ namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Cuboids
         /// Creates a textured <see cref="Cube"/> mesh, specifying a <see cref="Texture"/> for each side.
         /// </summary>
         /// <param name="worldOrigin">The position of the <see cref="Cube"/> in world space.</param>
-        /// <param name="worldOrientation"></param>
+        /// <param name="worldOrientation">The orientation of the <see cref="Cube"/> in world space.</param>
         /// <param name="sideLength">The length of each side of the <see cref="Cube"/>.</param>
         /// <param name="front">The <see cref="Texture"/> for the front face of the <see cref="Cube"/>.</param>
         /// <param name="right">The <see cref="Texture"/> for the right face of the <see cref="Cube"/>.</param>
@@ -108,41 +107,12 @@ namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Cuboids
                     Texture top,
                     Texture bottom) : base(worldOrigin, worldOrientation, 3)
         {
+            Vertices = MeshData.CuboidVertices;
+            Edges = MeshData.CuboidEdges;
+            Faces = MeshData.GenerateTextureFaces(new Texture[] { back, right, front, left, top, bottom });
+
             SideLength = sideLength;
             Textures = new Texture[6] { front, right, back, left, top, bottom };
-        }
-
-        private void SetStructure(float sideLength)
-        {
-            SideLength = sideLength;
-
-            Vertices = new Vertex[8]
-            {
-                new(new Vector4D(0, 0, 0, 1)), // 0
-                new(new Vector4D(1, 0, 0, 1)), // 1
-                new(new Vector4D(1, 1, 0, 1)), // 2
-                new(new Vector4D(0, 1, 0, 1)), // 3
-                new(new Vector4D(0, 0, 1, 1)), // 4
-                new(new Vector4D(1, 0, 1, 1)), // 5
-                new(new Vector4D(1, 1, 1, 1)), // 6
-                new(new Vector4D(0, 1, 1, 1)) // 7
-            }; // need to be oriented to front side
-
-            Edges = new Edge[12]
-            {
-                new(Vertices[0], Vertices[1]), // 0
-                new(Vertices[1], Vertices[2]), // 1
-                new(Vertices[2], Vertices[3]), // 2
-                new(Vertices[0], Vertices[3]), // 3
-                new(Vertices[1], Vertices[5]), // 4
-                new(Vertices[5], Vertices[6]), // 5
-                new(Vertices[2], Vertices[6]), // 6
-                new(Vertices[4], Vertices[5]), // 7
-                new(Vertices[4], Vertices[7]), // 8
-                new(Vertices[6], Vertices[7]), // 9
-                new(Vertices[0], Vertices[4]), // 10
-                new(Vertices[3], Vertices[7]) // 11
-            };
         }
 
         #endregion
