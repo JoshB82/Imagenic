d1af5d1aba88e3a18fc8e0d67822b7c8b6af985a
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-11-06 18:28:34 +0000

    Torus updates

--- 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Torus.cs
+++ 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Torus.cs
@@ -10,8 +10,10 @@
  * Defines a torus mesh.
  */
 
+using _3D_Engine.Entities.SceneObjects.Meshes.Components;
 using _3D_Engine.Maths;
 using _3D_Engine.Maths.Vectors;
+using static System.MathF;
 
 namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions
 {
@@ -22,11 +24,64 @@ namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions
     {
         #region Fields and Properties
 
-        public float InnerRadius { get; set; }
+        private float innerRadius, outerRadius;
+        private int innerResolution, outerResolution;
 
-        public float OuterRadius { get; set; }
+        public float InnerRadius
+        {
+            get => innerRadius;
+            set
+            {
+                if (value == innerRadius) return;
+                innerRadius = value;
+                RequestNewRenders();
 
-        public int Resolution { get; set; }
+                GenerateVertices();
+            }
+        }
+
+        public float OuterRadius
+        {
+            get => outerRadius;
+            set
+            {
+                if (value == outerRadius) return;
+                outerRadius = value;
+                RequestNewRenders();
+
+                GenerateVertices();
+            }
+        }
+
+        public int InnerResolution
+        {
+            get => innerResolution;
+            set
+            {
+                if (value == innerResolution) return;
+                innerResolution = value;
+                RequestNewRenders();
+
+                GenerateVertices();
+                GenerateEdges();
+                GenerateFaces();
+            }
+        }
+
+        public int OuterResolution
+        {
+            get => outerResolution;
+            set
+            {
+                if (value == outerResolution) return;
+                outerResolution = value;
+                RequestNewRenders();
+
+                GenerateVertices();
+                GenerateEdges();
+                GenerateFaces();
+            }
+        }
 
         #endregion
 
@@ -34,9 +89,50 @@ namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions
 
         public Torus(Vector3D worldOrigin,
                      Orientation worldOrientation,
-                     float radius,
                      float innerRadius,
-                     float outerRadius) : base(worldOrigin, worldOrientation, 3)
+                     float outerRadius,
+                     int innerResolution,
+                     int outerResolution) : base(worldOrigin, worldOrientation, 3)
+        {
+            this.innerRadius = innerRadius;
+            this.outerRadius = outerRadius;
+            this.innerResolution = innerResolution;
+            this.outerResolution = outerResolution;
+
+            GenerateVertices();
+            GenerateEdges();
+            GenerateFaces();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void GenerateVertices()
+        {
+            Content.Vertices = new Vertex[innerResolution * outerResolution];
+            Content.Vertices[0] = new Vertex(Vector4D.Zero);
+
+            float interiorRadius = (outerRadius - innerRadius) / 2;
+            float exteriorRadius = innerRadius + interiorRadius;
+            float innerAngle = 2 * PI / innerResolution;
+            float outerAngle = 2 * PI / outerResolution;
+            for (int i = 0; i < innerResolution; i++)
+            {
+                Content.Vertices[i + 1] = new Vertex(new Vector4D(Cos(innerAngle * i) * interiorRadius, Sin(innerAngle * i) * interiorRadius, 0, 1));
+            }
+            for (int i = 0; i < outerResolution; i++)
+            {
+                Content.Vertices[i + 1].Point = new Vector4D(Cos(outerAngle * i) * exteriorRadius * Content.Vertices[i + 1].Point.x, Content.Vertices[i + 1].Point.y, Sin(outerAngle * i) * exteriorRadius, 1);
+            }
+        }
+
+        private void GenerateEdges()
+        {
+
+        }
+
+        private void GenerateFaces()
         {
 
         }
