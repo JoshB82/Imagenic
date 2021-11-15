8189564f3fade39a2281ff8ab39677ce00668a52
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-11-15 22:26:59 +0000

    Updates

---
+++ 3D-Engine/Entities/Ray.cs
@@ -0,0 +1,33 @@
+﻿using _3D_Engine.Maths.Vectors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_Engine.Entities;
+
+public class Ray : Entity
+{
+    #region Fields and Properties
+
+    public Vector3D StartPosition { get; set; }
+
+    public Vector3D Direction { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public Ray(Vector3D startPosition, Vector3D direction)
+    {
+        StartPosition = startPosition;
+        Direction = direction;
+    }
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
