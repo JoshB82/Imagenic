8e62ce13a67bb41b908f940a3774c11ce78c45e5
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-11-20 12:09:23 +0000

    Render updates

--- 3D-Engine/Renderers/Rasteriser.cs
+++ 3D-Engine/Renderers/Rasteriser.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace _3D_Engine.Renderers
 {
     public class Rasteriser : Renderer
     {
+        #region Fields and Properties
+        
+        internal bool NewShadowMapNeeded { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public async override Task<Bitmap> Render()
+        {
+
+        }
+
+        #endregion
     }
 }
