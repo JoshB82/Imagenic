30bded06834c8ca583ac2a3a3ba6b4e0d02ca9f4
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-11-27 15:20:55 +0000

    Update mesh content

--- 3D-Engine/Entities/SceneObjects/Meshes/Components/MeshContent.cs
+++ 3D-Engine/Entities/SceneObjects/Meshes/Components/MeshContent.cs
@@ -1,14 +1,38 @@
-﻿using _3D_Engine.Entities.SceneObjects.Meshes.Components.Edges;
+﻿using _3D_Engine.Constants;
+using _3D_Engine.Entities.SceneObjects.Meshes.Components.Edges;
 using System.Collections.Generic;
 
 namespace _3D_Engine.Entities.SceneObjects.Meshes.Components
 {
     public class MeshContent
     {
-        public IList<Vertex> Vertices { get; set; }
+        #region Fields and Properties
+
+        private IList<Vertex> vertices;
+        public IList<Vertex> Vertices
+        {
+            get => vertices;
+            set
+            {
+                vertices = value ?? throw new ParameterCannotBeNullException();
+            }
+        }
         public IList<Edge> Edges { get; set; }
         public IList<Face> Faces { get; set; }
 
         public IEnumerable<Texture> Textures { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public MeshContent(IList<Vertex> vertices, IList<Edge> edges = null, IList<Face> faces = null)
+        {
+            Vertices = vertices;
+            Edges = edges;
+            Faces = faces;
+        }
+
+        #endregion
     }
 }
