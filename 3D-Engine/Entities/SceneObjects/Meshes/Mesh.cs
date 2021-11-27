bcb9b84fc5873b52498175a4cc6e01d554ee7c02
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-11-27 22:00:44 +0000

    Namespace updates

--- 3D-Engine/Entities/SceneObjects/Meshes/Mesh.cs
+++ 3D-Engine/Entities/SceneObjects/Meshes/Mesh.cs
@@ -21,153 +21,152 @@ using _3D_Engine.Maths.Transformations;
 using _3D_Engine.Maths.Vectors;
 using System.Collections.Generic;
 
-namespace _3D_Engine.Entities.SceneObjects.Meshes
+namespace _3D_Engine.Entities.SceneObjects.Meshes;
+
+/// <include file="Help_8.xml" path="doc/members/member[@name='T:_3D_Engine.Mesh']/*"/>
+public abstract partial class Mesh : SceneObject
 {
-    /// <include file="Help_8.xml" path="doc/members/member[@name='T:_3D_Engine.Mesh']/*"/>
-    public abstract partial class Mesh : SceneObject
+    #region Fields and Properties
+
+    // Structure
+    private MeshContent content = new MeshContent(new Vertex[] { new Vertex(Vector4D.UnitW) });
+    public MeshContent Content
     {
-        #region Fields and Properties
-
-        // Structure
-        private MeshContent content = new MeshContent(new Vertex[] { new Vertex(Vector4D.UnitW) });
-        public MeshContent Content
+        get => content;
+        set
         {
-            get => content;
-            set
-            {
-                content = value ?? throw new ParameterCannotBeNullException();
-            }
+            content = value ?? throw new ParameterCannotBeNullException();
         }
-        protected abstract IList<Vertex> GenerateVertices(MeshData meshData);
-        protected abstract IList<Edge> GenerateEdges(MeshData meshData);
-        protected abstract IList<Face> GenerateFaces(MeshData meshData);
-
-        /// <summary>
-        /// The <see cref="Vertex">vertices</see> in the <see cref="Mesh"/>.
-        /// </summary>
-        //public Vertex[] Vertices { get; protected set; }
-        /// <summary>
-        /// The <see cref="Edge">edges</see> in the <see cref="Mesh"/>.
-        /// </summary>
-        //public Edge[] Edges { get; protected set; }
-        /// <summary>
-        /// The <see cref="Triangle">triangles</see> in the <see cref="Mesh"/>.
-        /// </summary>
-        //public Triangle[] Triangles { get; internal set; }
-
-        // Appearance
-        private bool drawEdges;
-        /// <summary>
-        /// Determines if the <see cref="Mesh"> Mesh's</see> <see cref="Edge">Edges</see> are drawn.
-        /// </summary>
-        public bool DrawEdges
-        {
-            get => drawEdges;
-            set
-            {
-                if (value == drawEdges) return;
-                drawEdges = value;
-                RequestNewRenders();
-            }
-        }
-
-        private bool drawFaces;
-        /// <summary>
-        /// Determines if the<see cref="Mesh"> Mesh's</see> <see cref="Triangle">Faces</see> are drawn.
-        /// </summary>
-        public bool DrawFaces
-        {
-            get => drawFaces;
-            set
-            {
-                if (value == drawFaces) return;
-                drawFaces = value;
-                RequestNewRenders();
-            }
-        }
-
-        // Headed Rendering Object
-        internal List<RenderingObject> HeadedRenderingObjects { get; set; } = new();
-        internal override void RequestNewRenders()
-        {
-            base.RequestNewRenders();
-
-            foreach (RenderingObject renderingObject in HeadedRenderingObjects)
-            {
-                foreach (Camera camera in renderingObject.RenderCameras)
-                {
-                    camera.NewRenderNeeded = true;
-                }
-            }
-        }
-
-        // Textures
-        /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Textures']/*"/>
-        public Texture[] Textures { get; internal set; }
-        /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Has_Texture']/*"/>
-        public bool HasTexture { get; protected set; } = false;
-
-        // Miscellaneous
-        /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Dimension']/*"/>
-        public int Dimension { get; }
-        /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Casts_Shadows']/*"/>
-        public bool CastsShadows { get; set; } = true;
-        /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Draw_Outline']/*"/>
-        public bool DrawOutline { get; set; } = false;
-        public float Opacity { get; set; } = 1f;
-
-        // Matrices and Vectors
-        internal override void CalculateModelToWorldMatrix()
-        {
-            base.CalculateModelToWorldMatrix();
-            ModelToWorld *= Transform.Scale(Scaling);
-        }
-
-        private Vector3D scaling = Vector3D.One;
-        public Vector3D Scaling
-        {
-            get => scaling;
-            set
-            {
-                if (value == scaling) return;
-                scaling = value;
-                CalculateModelToWorldMatrix();
-                RequestNewRenders();
-            }
-        }
-
-        #endregion
-
-        #region Constructors
-
-        protected Mesh(Vector3D worldOrigin,
-                       Orientation worldOrientation,
-                       int dimension,
-                       bool hasDirectionArrows = true) : base(worldOrigin, worldOrientation, hasDirectionArrows)
-        {
-            if (worldOrientation is null)
-            {
-                // throw exception
-            }
-            if (dimension >= 0 && dimension <= 3)
-            {
-                Dimension = dimension;
-            }
-            else
-            {
-                // throw exception
-            }
-
-            DrawEdges = Content.Edges is not null;
-            DrawFaces = Content.Faces is not null;
-        }
-
-        #endregion
-
-        #region Classes
-
-        protected abstract class MeshData { }
-
-        #endregion
     }
+    protected abstract IList<Vertex> GenerateVertices(MeshData meshData);
+    protected abstract IList<Edge> GenerateEdges(MeshData meshData);
+    protected abstract IList<Face> GenerateFaces(MeshData meshData);
+
+    /// <summary>
+    /// The <see cref="Vertex">vertices</see> in the <see cref="Mesh"/>.
+    /// </summary>
+    //public Vertex[] Vertices { get; protected set; }
+    /// <summary>
+    /// The <see cref="Edge">edges</see> in the <see cref="Mesh"/>.
+    /// </summary>
+    //public Edge[] Edges { get; protected set; }
+    /// <summary>
+    /// The <see cref="Triangle">triangles</see> in the <see cref="Mesh"/>.
+    /// </summary>
+    //public Triangle[] Triangles { get; internal set; }
+
+    // Appearance
+    private bool drawEdges;
+    /// <summary>
+    /// Determines if the <see cref="Mesh"> Mesh's</see> <see cref="Edge">Edges</see> are drawn.
+    /// </summary>
+    public bool DrawEdges
+    {
+        get => drawEdges;
+        set
+        {
+            if (value == drawEdges) return;
+            drawEdges = value;
+            RequestNewRenders();
+        }
+    }
+
+    private bool drawFaces;
+    /// <summary>
+    /// Determines if the<see cref="Mesh"> Mesh's</see> <see cref="Triangle">Faces</see> are drawn.
+    /// </summary>
+    public bool DrawFaces
+    {
+        get => drawFaces;
+        set
+        {
+            if (value == drawFaces) return;
+            drawFaces = value;
+            RequestNewRenders();
+        }
+    }
+
+    // Headed Rendering Object
+    internal List<RenderingObject> HeadedRenderingObjects { get; set; } = new();
+    internal override void RequestNewRenders()
+    {
+        base.RequestNewRenders();
+
+        foreach (RenderingObject renderingObject in HeadedRenderingObjects)
+        {
+            foreach (Camera camera in renderingObject.RenderCameras)
+            {
+                camera.NewRenderNeeded = true;
+            }
+        }
+    }
+
+    // Textures
+    /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Textures']/*"/>
+    public Texture[] Textures { get; internal set; }
+    /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Has_Texture']/*"/>
+    public bool HasTexture { get; protected set; } = false;
+
+    // Miscellaneous
+    /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Dimension']/*"/>
+    public int Dimension { get; }
+    /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Casts_Shadows']/*"/>
+    public bool CastsShadows { get; set; } = true;
+    /// <include file="Help_8.xml" path="doc/members/member[@name='P:_3D_Engine.Mesh.Draw_Outline']/*"/>
+    public bool DrawOutline { get; set; } = false;
+    public float Opacity { get; set; } = 1f;
+
+    // Matrices and Vectors
+    internal override void CalculateModelToWorldMatrix()
+    {
+        base.CalculateModelToWorldMatrix();
+        ModelToWorld *= Transform.Scale(Scaling);
+    }
+
+    private Vector3D scaling = Vector3D.One;
+    public Vector3D Scaling
+    {
+        get => scaling;
+        set
+        {
+            if (value == scaling) return;
+            scaling = value;
+            CalculateModelToWorldMatrix();
+            RequestNewRenders();
+        }
+    }
+
+    #endregion
+
+    #region Constructors
+
+    protected Mesh(Vector3D worldOrigin,
+                    Orientation worldOrientation,
+                    int dimension,
+                    bool hasDirectionArrows = true) : base(worldOrigin, worldOrientation, hasDirectionArrows)
+    {
+        if (worldOrientation is null)
+        {
+            // throw exception
+        }
+        if (dimension >= 0 && dimension <= 3)
+        {
+            Dimension = dimension;
+        }
+        else
+        {
+            // throw exception
+        }
+
+        DrawEdges = Content.Edges is not null;
+        DrawFaces = Content.Faces is not null;
+    }
+
+    #endregion
+
+    #region Classes
+
+    protected abstract class MeshData { }
+
+    #endregion
 }
