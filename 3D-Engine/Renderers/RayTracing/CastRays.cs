0c363f0c8c15b77b8081cd5e73f719ff8bca601a
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-12-12 21:19:07 +0000

    Updates

--- 3D-Engine/Renderers/RayTracing/CastRays.cs
+++ 3D-Engine/Renderers/RayTracing/CastRays.cs
@@ -2,9 +2,9 @@
 using _3D_Engine.Entities.SceneObjects;
 using _3D_Engine.Entities.SceneObjects.Meshes.Components.Faces;
 using _3D_Engine.Entities.SceneObjects.RenderingObjects.Cameras;
+using _3D_Engine.Entities.SceneObjects.RenderingObjects.Lights;
 using _3D_Engine.Entities.SceneObjects.RenderingObjects.Rendering;
 using _3D_Engine.Maths.Vectors;
-using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -17,37 +17,28 @@ namespace _3D_Engine.Renderers.RayTracing
     {
         internal const int maxRayCount = 5;
 
-        internal static async Task<Image> CastRays(IEnumerable<Triangle> triangles, Camera camera)
+        internal static async Task<Buffer2D<Color>> CastRays(IEnumerable<Triangle> triangles, IEnumerable<Light> lights, Camera camera)
         {
             SceneObject scene = sceneObject.DeepCopy(); // ??
             sceneObject.RemoveChildren(x => !x.Visible || x is Camera); // ??
 
             Buffer2D<Task<bool>> taskBuffer = new Buffer2D<Task<bool>>(camera.RenderWidth, camera.RenderHeight);
-
+            Buffer2D<Color> colourBuffer = new Buffer2D<Color>(camera.RenderWidth, camera.RenderHeight);
 
             taskBuffer.ForEach(task =>
             {
                 task = Task.Factory.StartNew(() =>
                 {
-                    return CastRay();
+                    if (CastRay(triangles, ))
+                    {
+                        colourBuffer.Values[][] = ;
+                    }
                 });
             });
 
             Task.WaitAll((Task<bool>[])taskBuffer);
 
-            for (int i = 0; i < camera.RenderWidth; i++)
-            {
-                for (int j = 0; j < camera.RenderHeight; j++)
-                {
-                    
-                    //CastRay(camera.WorldOrigin, );
-
-                    //tasks[i].ContinueWith(task =>
-                    //{
-
-                    //});
-                }
-            }
+            return colourBuffer;
         }
 
         internal static bool CastRay(IEnumerable<Triangle> triangles, Vector3D point1, Vector3D point2, out int rayCount)
