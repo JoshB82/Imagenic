4f4ca779a689dae7fd28f9b30bc75aa66f8de5ce
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-12-30 19:10:32 +0000

    Updates

--- 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Spheres/LatLongSphere.cs
+++ 3D-Engine/Entities/SceneObjects/Meshes/ThreeDimensions/Spheres/LatLongSphere.cs
@@ -8,100 +8,122 @@ using System;
 using System.Collections.Generic;
 using static System.MathF;
 
-namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Spheres
+namespace _3D_Engine.Entities.SceneObjects.Meshes.ThreeDimensions.Spheres;
+
+public sealed class LatLongSphere : Sphere
 {
-    public sealed class LatLongSphere : Sphere
+    #region Fields and Properties
+
+    private int latResolution, longResolution;
+    public int LatResolution
     {
-        #region Fields and Properties
-
-        public int LatResolution { get; set; }
-        public int LongResolution { get; set; }
-
-        #endregion
-
-        #region Constructors
-
-        public LatLongSphere(Vector3D worldOrigin, Orientation worldOrientation, float radius, int latResolution, int longResolution) : base(worldOrigin, worldOrientation, GenerateStructure(latResolution, longResolution), radius)
+        get => latResolution;
+        set
         {
-            LatResolution = latResolution;
-            LongResolution = longResolution;
+            if (value == latResolution) return;
+            latResolution = value;
+            InvokeRenderingEvents();
         }
-
-        #endregion
-
-        #region Methods
-
-        private static MeshStructure GenerateStructure(int latResolution, int longResolution)
-        {
-            IList<Vertex> vertices = GenerateVertices(latResolution, longResolution);
-            IList<Edge> edges = GenerateEdges();
-            IList<Face> faces = GenerateFaces();
-
-            return new MeshStructure(Dimension.Three, vertices, edges, faces);
-        }
-
-        private static IList<Vertex> GenerateVertices(int latResolution, int longResolution)
-        {    
-            IList<Vertex> vertices = new Vertex[latResolution * longResolution];
-
-            float latAngleStep = Tau / latResolution;
-            float longAngleStep = PI / longResolution;
-
-            for (int i = 0; i < longResolution; i++)
-            {
-                float longAngle = longAngleStep * i - PI / 2;
-
-                for (int j = 0; j < latResolution; j++)
-                {
-                    float latAngle = latAngleStep * j;
-
-                    float x = Cos(latAngle) * Cos(longAngle);
-                    float y = Sin(latAngle) * Cos(longAngle);
-                    float z = Sin(longAngle);
-
-                    vertices[i * longResolution + j] = new Vertex(new Vector4D(x, y, z, 1));
-                }
-            }
-
-                    
-                
-                    
-                
-                    
-                default:
-                    throw new MessageBuilder<ParameterNotSupportedMessage>()
-                        .AddParameters(nameof(construction))
-                        .BuildIntoException<ArgumentException>();
-
-            
-
-            return null; // TODO: Finish
-        }
-
-        private static IList<Edge> GenerateEdges()
-        {
-            IList<Edge> edges = new Edge[];
-        }
-
-        private static IList<Face> GenerateFaces()
-        {
-            IList<Face> faces = new Face[];
-        }
-
-        #endregion
-
-        #region Casting
-
-        public static implicit operator Icosphere(LatLongSphere sphere)
-        {
-
-        }
-
-        public static implicit operator CubeSphere(LatLongSphere sphere)
-        {
-
-        }
-
-        #endregion
     }
+    public int LongResolution
+    {
+        get => longResolution;
+        set
+        {
+            if (value == longResolution) return;
+            longResolution = value;
+            InvokeRenderingEvents();
+        }
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public LatLongSphere(Vector3D worldOrigin, Orientation worldOrientation, float radius, int latResolution, int longResolution) : base(worldOrigin, worldOrientation, GenerateStructure(latResolution, longResolution), radius)
+    {
+        LatResolution = latResolution;
+        LongResolution = longResolution;
+    }
+
+    #endregion
+
+    #region Methods
+
+    private static MeshStructure GenerateStructure(int latResolution, int longResolution)
+    {
+        IList<Vertex> vertices = GenerateVertices(latResolution, longResolution);
+        IList<Edge> edges = GenerateEdges(vertices, latResolution, longResolution);
+        IList<Face> faces = GenerateFaces();
+
+        return new MeshStructure(Dimension.Three, vertices, edges, faces);
+    }
+
+    private static IList<Vertex> GenerateVertices(int latResolution, int longResolution)
+    {    
+        IList<Vertex> vertices = new Vertex[latResolution * longResolution];
+
+        float latAngleStep = Tau / latResolution;
+        float longAngleStep = PI / longResolution;
+
+        for (int i = 0; i < longResolution; i++)
+        {
+            float longAngle = longAngleStep * i - PI / 2;
+
+            for (int j = 0; j < latResolution; j++)
+            {
+                float latAngle = latAngleStep * j;
+
+                float x = Cos(latAngle) * Cos(longAngle);
+                float y = Sin(latAngle) * Cos(longAngle);
+                float z = Sin(longAngle);
+
+                vertices[i * longResolution + j] = new Vertex(new Vector3D(x, y, z));
+            }
+        }
+
+        return vertices;
+
+        //throw new MessageBuilder<ParameterNotSupportedMessage>()
+            //.AddParameters(nameof(construction))
+            //.BuildIntoException<ArgumentException>();
+    }
+
+    private static IList<Edge> GenerateEdges(IList<Vertex> vertices, int latResolution, int longResolution)
+    {
+        IList<Edge> edges = new Edge[latResolution * longResolution];
+
+        for (int i = 0; i < longResolution; i++)
+        {
+            for (int j = 0; j < latResolution; j++)
+            {
+                edges[i * longResolution + j] = new SolidEdge();
+            }
+        }
+
+        return edges;
+    }
+
+    private static IList<Face> GenerateFaces()
+    {
+        IList<Face> faces = new Face[];
+
+        return faces;
+    }
+
+    #endregion
+
+    #region Casting
+
+    public static implicit operator Icosphere(LatLongSphere sphere)
+    {
+        return new Icosphere();
+    }
+
+    public static implicit operator CubeSphere(LatLongSphere sphere)
+    {
+        return new CubeSphere();
+    }
+
+    #endregion
 }
---
+++ 3D-Engine/Utilities/EventList.cs
@@ -0,0 +1,81 @@
+﻿using _3D_Engine.Entities;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _3D_Engine.Utilities;
+
+public class EventList<T> : Entity, IList<T>
+{
+    private IList<T> internalList = new List<T>();
+    public bool ActivateRenderingEvent { get; set; } = true;
+    public bool ActivateShadowMapEvent { get; set; } = true;
+
+    public T this[int index]
+    {
+        get => internalList[index];
+        set
+        {
+            internalList[index] = value;
+            InvokeRenderingEvents(ActivateRenderingEvent, ActivateShadowMapEvent);
+        }
+    }
+
+    public int Count => internalList.Count;
+
+    public bool IsReadOnly => internalList.IsReadOnly;
+
+    public void Add(T item)
+    {
+        internalList.Add(item);
+        InvokeRenderingEvents(ActivateRenderingEvent, ActivateShadowMapEvent);
+    }
+
+    public void Clear()
+    {
+        internalList.Clear();
+        InvokeRenderingEvents(ActivateRenderingEvent, ActivateShadowMapEvent);
+    }
+
+    public bool Contains(T item)
+    {
+        return internalList.Contains(item);
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        internalList.CopyTo(array, arrayIndex);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return internalList.GetEnumerator();
+    }
+
+    public int IndexOf(T item)
+    {
+        return internalList.IndexOf(item);
+    }
+
+    public void Insert(int index, T item)
+    {
+        internalList.Insert(index, item);
+        InvokeRenderingEvents(ActivateRenderingEvent, ActivateShadowMapEvent);
+    }
+
+    public bool Remove(T item)
+    {
+        InvokeRenderingEvents(ActivateRenderingEvent, ActivateShadowMapEvent);
+        return internalList.Remove(item);
+    }
+
+    public void RemoveAt(int index)
+    {
+        internalList.RemoveAt(index);
+        InvokeRenderingEvents(ActivateRenderingEvent, ActivateShadowMapEvent);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)internalList).GetEnumerator();
+    }
+}
