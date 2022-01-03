af09e9d02a8e545c3f86f6fc714a6a9fa4bdb217
Josh Bryant <jbryantjersey@hotmail.co.uk>
2022-01-03 19:02:25 +0000

    Updates

---
+++ 3D-Engine/Utilities/ColourHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_Engine.Utilities;
+
+internal static class ColourHelper
+{
+    public static Color MixColours(Color[] colours)
+    {
+        Color colourMix = colours[0];
+        for (int i = 1; i < colours.Length; i++)
+        {
+            colourMix += colours[i];
+        } 
+        return colourMix;
+    }
+}
