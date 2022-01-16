4561d2d95983d75ce4e9a5fe09dbc31de424c5c5
Josh Bryant <jbryantjersey@hotmail.co.uk>
2022-01-16 22:36:23 +0000

    Updates

--- 3D-Engine/Maths/Vectors/VectorExtensions.cs
+++ 3D-Engine/Maths/Vectors/VectorExtensions.cs
@@ -9,10 +9,12 @@ namespace Imagenic.Core.Maths.Vectors;
 
 internal static class VectorExtensions
 {
+    private readonly static Vector3DApproximateEqualityComparer comparer = new Vector3DApproximateEqualityComparer();
+
     public static bool AreCollinear(this IEnumerable<Vector3D> points)
     {
         return points.Zip(points.Skip(1), (a, b) => (b - a).Normalise())
-            .Distinct().Count() == 1;
+            .Distinct(comparer).Count() == 1;
     }
 
     public static float CalculateGradient(Vector2D point1, Vector2D point2)
