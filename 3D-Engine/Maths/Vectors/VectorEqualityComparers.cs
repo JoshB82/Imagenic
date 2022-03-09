b656875b6d8a0911e9ab24d4ca0fe3059f4b48ee
Josh Bryant <jbryantjersey@hotmail.co.uk>
2022-03-09 22:25:12 +0000

    Updates

--- 3D-Engine/Maths/Vectors/VectorEqualityComparers.cs
+++ 3D-Engine/Maths/Vectors/VectorEqualityComparers.cs
@@ -1,40 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Imagenic.Core.Maths.Vectors;
 
 public class Vector2DApproximateEqualityComparer : IEqualityComparer<Vector2D>
 {
+    #region Fields and Properties
+
+    public float Epsilon { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public Vector2DApproximateEqualityComparer(float epsilon = float.Epsilon)
+    {
+        Epsilon = epsilon;
+    }
+
+    #endregion
+
+    #region Methods
+
     public bool Equals(Vector2D x, Vector2D y)
     {
-        return x.ApproxEquals(y);
+        return x.ApproxEquals(y, Epsilon);
     }
 
     public int GetHashCode([DisallowNull] Vector2D obj)
     {
         throw new NotImplementedException();
     }
+
+    #endregion
 }
 
 public class Vector3DApproximateEqualityComparer : IEqualityComparer<Vector3D>
 {
+    #region Fields and Properties
+
+    public float Epsilon { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public Vector3DApproximateEqualityComparer(float epsilon = float.Epsilon)
+    {
+        Epsilon = epsilon;
+    }
+
+    #endregion
+
+    #region Methods
+
     public bool Equals(Vector3D x, Vector3D y)
     {
-        return x.ApproxEquals(y);
+        return x.ApproxEquals(y, Epsilon);
     }
 
     public int GetHashCode([DisallowNull] Vector3D obj)
     {
         throw new NotImplementedException();
     }
+
+    #endregion
 }
 
 public class Vector4DApproximateEqualityComparer : IEqualityComparer<Vector4D>
 {
+    #region Fields and Properties
+
+    public float Epsilon { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public Vector4DApproximateEqualityComparer(float epsilon = float.Epsilon)
+    {
+        Epsilon = epsilon;
+    }
+
+    #endregion
+
+    #region Methods
+
     public bool Equals(Vector4D x, Vector4D y)
     {
         return x.ApproxEquals(y);
@@ -44,4 +96,6 @@ public class Vector4DApproximateEqualityComparer : IEqualityComparer<Vector4D>
     {
         throw new NotImplementedException();
     }
+
+    #endregion
 }
