eee66491e91981264b4ff03975ee7fd968888bab
Josh Bryant <jbryantjersey@hotmail.co.uk>
2022-03-13 18:06:29 +0000

    Added TestDataHelper

---
+++ Unit Tests/TestDataHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unit_Tests;
+
+internal static class TestDataHelper
+{
+    private static readonly Random random = new();
+
+    internal static IEnumerable<int> GenerateRandomInts(int numberToGenerate, int rangeLowest, int rangeHighest)
+    {
+        int[] result = new int[numberToGenerate];
+        return result.Select(x => random.Next(rangeLowest, rangeHighest));
+    }
+
+    internal static IEnumerable<float> GenerateRandomFloats(int numberToGenerate, float rangeLowest, float rangeHighest)
+    {
+        float[] result = new float[numberToGenerate];
+        float rangeDifference = rangeHighest - rangeLowest;
+        return result.Select(x => random.NextSingle() * rangeDifference + rangeLowest);
+    }
+}
