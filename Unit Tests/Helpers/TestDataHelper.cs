eb28747005a9c0c1bfd1c93a6b3ff19d376ee4d4
Josh Bryant <jbryantjersey@hotmail.co.uk>
2022-03-16 22:11:42 +0000

    Updates

--- Unit Tests/Helpers/TestDataHelper.cs
+++ Unit Tests/Helpers/TestDataHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Imagenic.Core.Maths.Vectors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,10 +15,29 @@ internal static class TestDataHelper
         return result.Select(x => random.Next(rangeLowest, rangeHighest));
     }
 
+    internal static float GenerateRandomFloat(float rangeLowest, float rangeHighest) => random.NextSingle() * (rangeHighest - rangeLowest) + rangeLowest;
+
     internal static IEnumerable<float> GenerateRandomFloats(int numberToGenerate, float rangeLowest, float rangeHighest)
     {
         float[] result = new float[numberToGenerate];
-        float rangeDifference = rangeHighest - rangeLowest;
-        return result.Select(x => random.NextSingle() * rangeDifference + rangeLowest);
+        return result.Select(x => GenerateRandomFloat(rangeLowest, rangeHighest));
+    }
+
+    internal static IEnumerable<Vector2D> GenerateRandomVector2Ds(int numberToGenerate, float rangeLowest, float rangeHighest)
+    {
+        Vector2D[] result = new Vector2D[numberToGenerate];
+        return result.Select(x => new Vector2D(GenerateRandomFloats(2, rangeLowest, rangeHighest)));
+    }
+
+    internal static IEnumerable<Vector3D> GenerateRandomVector3Ds(int numberToGenerate, float rangeLowest, float rangeHighest)
+    {
+        Vector3D[] result = new Vector3D[numberToGenerate];
+        return result.Select(x => new Vector3D(GenerateRandomFloats(3, rangeLowest, rangeHighest)));
+    }
+
+    internal static IEnumerable<Vector4D> GenerateRandomVector4Ds(int numberToGenerate, int rangeLowest, int rangeHighest)
+    {
+        Vector4D[] result = new Vector4D[numberToGenerate];
+        return result.Select(x => new Vector4D(GenerateRandomFloats(4, rangeLowest, rangeHighest)));
     }
 }
--- Unit Tests/Tests/Matrix4x4Tests.cs
+++ Unit Tests/Tests/Matrix4x4Tests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Imagenic.UnitTests.Tests;
 
-class Matrix4x4Tests
+[TestClass]
+public class Matrix4x4Tests
 {
 }
