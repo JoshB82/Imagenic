c1a012c78478a3b645ff34153b900251dc6e6790
Josh Bryant <jbryantjersey@hotmail.co.uk>
2022-05-04 20:39:05 +0100

    Rearrange usings

--- 3D-Engine/Utilities/Global.cs
+++ 3D-Engine/Utilities/Global.cs
@@ -1,3 +1,4 @@
-﻿global using Imagenic.Core.Maths.Vectors;
-global using static System.MathF;
+﻿global using Imagenic.Core.Maths;
+global using Imagenic.Core.Maths.Vectors;
 global using static Imagenic.Core.Utilities.ExceptionHelper;
+global using static System.MathF;
