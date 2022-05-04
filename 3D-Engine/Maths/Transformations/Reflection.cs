013b25590585594442922e519646ff3c7be8280c
Josh Bryant <jbryantjersey@hotmail.co.uk>
2022-05-04 20:56:34 +0100

    Add beginning of file for reflection matrix generation code.

---
+++ 3D-Engine/Maths/Transformations/Reflection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imagenic.Core.Maths.Transformations;
+
+public static partial class Transform
+{
+    public static Matrix4x4 ReflectAboutPlane(Vector3D axis)
+    {
+        if (axis.ApproxEquals(Vector3D.Zero))
+        {
+            // throw exception
+        }
+
+        //return new();
+    }
+}
